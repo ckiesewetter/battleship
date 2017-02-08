7f2778b743c0bfa88a4e04425b1d4416bc539957
Noor Ghamedi <ghamedina@gmail.com>
2017-02-08 13:40:23 -0800

    Second Commit

--- controller.js
+++ controller.js
@@ -1,4 +1,4 @@
-$(document).ready (function(){
+$(document).ready(function(){
   function buildTable() {
     //variable keeps track of current row
     var currentRow = 0;
@@ -10,31 +10,45 @@ $(document).ready (function(){
         currentRow = i/10; //update current row
         $("#board").append('<tr id="row' + currentRow + '"</tr>'); //create a new table row with id "row<currentRow>"
         }
-        $("#row"+ currentRow).append("<td id=c" + i + "></td>"); //makes a new table cell with id "index<i>" under "row<currentRow"
+        $("#row"+ currentRow).append("<td data-index = " + i + "></td>"); //makes a new table cell with id "index<i>" under "row<currentRow"
        }
   }
+  // call function to build the table
   buildTable();
-});
-
-//when we click on each bix, it will change color.
-
-$(document).ready (function() {
+  placeShips();
+  var clickCounter = 0;
+  // create an onclick event to torpedo a ship
   $("td").on("click", function(){
     $(this).addClass("torpedo");
-  })
-});
+    // we need to call a var that refers to the rows/columns in the html
+    var squared = $(this).data("index").toString();
+
+    var row = squared[0];
+    var col = squared[1];
+    // and then plug the var into hitShip
+    hitShip(row, col);
+    if (clickCounter < 25) {
+      clickCounter = clickCounter + 1;
+    }
+    console.log(clickCounter);
+  });
+  console.log(board);
+
+});//doc.ready
+
 
 // Counter for torpedo
-//Each time the player clicks, subtract 1 from 25 counter.
-// var counter = 0
-//   function torpedoCount(){
-//     while (counter <= 25) {
-//       console.log(counter++);
-//         $("torpedoCount").html("You have " + counter + "torpedos!")
-//     }
-//   }
-  //
-    // 
+// Each time the player clicks, subtract 1 from 25 counter.
+
+  // function torpedoCount(){
+  //   var counter = 0
+  //   while (counter <= 25) {
+  //     console.log(counter++);
+  //       $("torpedoCount").html("You have " + counter + "torpedos!")
+  //   }
+  // }
+
+    //
     // $("td").on("click",function(){
     //   var counter = 0
     //     function torpedoCount(){
--- model.js
+++ model.js
@@ -1,53 +1,52 @@
-function newGame(){
-  var board = [
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0],
-    [0,0,0,0,0,0,0,0,0,0,0,0]
+var board = [
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0],
+    [0,0,0,0,0,0,0,0,0,0]
   ];
 
-  var miss = -1;
-  var ship = 1;
-  return {
-    torpedoCount: function(){
-      var counter = 1;
-        if (counter <= 25) {
-        console.log("you have used " + (counter++) +  " " + "torpedos");
-      } else {
-        console.log(counter);
-      }
-      },
-    returnBoard: function(){
-      return board;
-      },
-    doubleShot: function(trythis) {
-      if (trythis == ship) {
-        alert ("You already hit the ship");
-      } else if (trythis == miss) {
-        alert ("You can't choose this square");
-      }
-      },
-    placeShips: function() {
-      for (var i = 0; i <= 5; i++) {
-        // var random row = random number
-        var row = Math.floor( (Math.random() * 9) + 1);
-        // var random col = random number
-        var column = Math.floor( (Math.random() * 9) + 1);
-        // loop through board, check if each place has ship
-        // For each index
-        while (board[row][column] == 1) {
-          row = Math.floor( (Math.random() * 9) + 1);
-          column = Math.floor( (Math.random() * 9) + 1);
-        }
-        board[row][column] = ship;
-      }
-    }
-  };
+var miss = -1;
+var ship = 1;
+var hit = 2;
+
+function placeShips(){
+  for (var i = 0; i <= 5; i++) {
+   // var random row = random number
+   var row = Math.floor (Math.random() * 10);
+   // var random col = random number
+   var column = Math.floor (Math.random() * 10);
+   // loop through board, check if each place has ship
+   //commented out the while loop, it was giving us an extra 1 (ship).
+    // while (board[row][column] == 1) {
+    //   row = Math.floor (Math.random() * 10);
+    //   column = Math.floor (Math.random() * 10);
+    // }
+   board[row][column] = ship;
+ }
 }
-///stopped before #6
+    function returnBoard(){
+    return board;
+  }
+
+function hitShip(row, col){
+  //place a var inside this that represents the square we hit
+    if ( board[row][col] === 0) {
+      //reassign the empty square to a miss (-1)
+       board[row][col] = miss;
+      alert ("You miss!");
+    } else if (board[row][col] == 1) {
+      //reassign the 1 to a hit which we can call 2
+      board[row][col] = 2;
+      alert ("You torpedoed a ship!! Awesome!");
+    } else if (board[row][col] == -1){
+      alert("You already tried here, there's no ship!");
+    } else if (board[row][col] == 2) {
+      alert ("You already torpedoed this ship, yo!");
+    }
+  }
